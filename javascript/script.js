eda021faa0d6e0540d970191da0aa110292a7a67
Barney Meekin <BarneyJMeekin@gmail.com>
2016-09-17 17:58:46 +1200

    updated index.html, plus css and js

---
+++ javascript/script.js
@@ -0,0 +1,55 @@
+//when html loads
+var rowNumber = 16;
+var sideSize = 600 / rowNumber;
+var elementNumber = " ";
+for (var i = 0; i < rowNumber * rowNumber; i++){
+    elementNumber += "<div class = 'square'></div>";
+}
+$(document).ready(function() {
+    $('body').find("#container").append(elementNumber);
+    $('.square').css({"height": sideSize + 'px', "width": sideSize + 'px'});
+    $(document).on('mouseenter', '.square', function() {
+        $(this).css("background-color", "blue");
+    });
+});
+//when new grid button is clicked
+function makeGrid(){
+    var rowNumber = prompt("How many rows on the grid?");
+    var sideSize = 600 / rowNumber;
+    var elementNumber = " ";
+    for (var i = 0; i < rowNumber * rowNumber; i++){
+    elementNumber += "<div class = 'square'></div>";
+    }
+    $(document).ready(function() {
+        $('.square').remove();
+        $('body').find("#container").append(elementNumber);
+        $('.square').css({"height": sideSize + 'px', "width": sideSize + 'px'});
+        $(document).on('mouseenter', '.square', function() {
+            $(this).css("background-color", "blue");
+        });
+    });
+}
+//when random grid button is clicked
+function makeRandomGrid(){
+    var rowNumber = prompt("How many rows on the grid?");
+    var sideSize = 600 / rowNumber;
+    var elementNumber = " ";
+    for (var i = 0; i < rowNumber * rowNumber; i++){
+    elementNumber += "<div class = 'square'></div>";
+    }
+    function getRandomColor() {
+        function c() {
+          var hex = Math.floor(Math.random()*256).toString(16);
+          return ("0"+String(hex)).substr(-2); // pad with zero
+        }
+        return "#"+c()+c()+c();
+      }
+    $(document).ready(function() {
+        $('.square').remove();
+        $('body').find("#container").append(elementNumber);
+        $('.square').css({"height": sideSize + 'px', "width": sideSize + 'px'});
+        $(document).on('mouseenter', '.square', function() {
+            $(this).css("background-color", getRandomColor);
+        });
+    });
+}
