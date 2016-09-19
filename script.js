24ffd2f83cc5ac4dfdab57aa79ac6bc487403337
Barney Meekin <BarneyJMeekin@gmail.com>
2016-09-19 15:03:46 +1200

    Added comments

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //when html loads
 var rowNumber = 16;
-var sideSize = 700 / rowNumber;
+var sideSize = 700 / rowNumber;//calculates element size
 var elementNumber = " ";
 for (var i = 0; i < rowNumber * rowNumber; i++){
     elementNumber += "<div class = 'square'></div>";
 } //counts amount of elements
 $(document).ready(function() {
-    $('body').find("#container").append(elementNumber);
+    $('body').find("#container").append(elementNumber);//adds elements
     $('.square').css({"height": sideSize + 'px', "width": sideSize + 'px'});
     $(document).on('mouseenter', '.square', function() {
-        $(this).css("background-color", "#255E69");
+        $(this).css("background-color", "#255E69");//changes colour on event
     });
 });
 //when new grid button is clicked
@@ -21,7 +21,7 @@ function makeGrid(){
     elementNumber += "<div class = 'square'></div>";
     }
     $(document).ready(function() {
-        $('.square').remove();
+        $('.square').remove(); //clears grid 
         $('body').find("#container").append(elementNumber);
         $('.square').css({"height": sideSize + 'px', "width": sideSize + 'px'});
         $(document).on('mouseenter', '.square', function() {
@@ -37,7 +37,7 @@ function makeRandomGrid(){
     for (var i = 0; i < rowNumber * rowNumber; i++){
     elementNumber += "<div class = 'square'></div>";
     }
-    function getRandomColor() {
+    function getRandomColor() { //generates random color
         function c() {
           var hex = Math.floor(Math.random()*256).toString(16);
           return ("0"+String(hex)).substr(-2); // pad with zero
